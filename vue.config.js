9c470321a71c1077935e023ddaf82a7ce3060874
吴旭飞 <22373304@buaa.edu.cn>
2024-12-08 23:43:56 +0800

    modify icon

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,13 @@ module.exports = defineConfig({
     },
   },
   productionSourceMap: false,
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
+  }
 })
